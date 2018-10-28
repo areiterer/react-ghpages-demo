0de47a91ade7f0f3878d5f9527b72f3955fb5345
Andreas Reiterer <reiterer.a@gmail.com>
2018-10-28 13:41:05 +0100

    Updates

---
+++ precache-manifest.29f3a8adc2f41dfb992435d47b17a071.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ghpages-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "73d77b87c6b5bdbf4fbf",
+    "url": "/react-ghpages-demo/static/js/runtime~main.73d77b87.js"
+  },
+  {
+    "revision": "872a7a6ab1974be53902",
+    "url": "/react-ghpages-demo/static/js/main.872a7a6a.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/react-ghpages-demo/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "872a7a6ab1974be53902",
+    "url": "/react-ghpages-demo/static/css/main.a1df2559.chunk.css"
+  },
+  {
+    "revision": "6040366e35512ae6e63b283cfabfc0d7",
+    "url": "/react-ghpages-demo/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/react-ghpages-demo/precache-manifest.29f3a8adc2f41dfb992435d47b17a071.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-ghpages-demo/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
